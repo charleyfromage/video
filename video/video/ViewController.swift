229f389d4d788528e1b6c9d690759ddd4bb41394
charley <charley.fromage@gmail.com>
2019-11-13 15:03:21 +0100

    Initial commit

---
+++ video/video/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  video
+//
+//  Created by Fromage Charley on 13/11/2019.
+//  Copyright © 2019 Voodoo Coding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
